a4c32b63902a13d19dd3645b39064fa33c807c4f
johnherrera88 <john.herrera88@gmail.com>
2021-09-20 14:26:30 -0600

    Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "John";
+    int age = 30;
+    String state = "State";
+
+    /*
+      System.out.println("Name: " + name + "Age: " + age + "State: " + state);
+    */
+  }
+}
