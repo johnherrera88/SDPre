e61f5f5fb16ee984393fdf5ad145f2c9a5bb634d
johnherrera88 <john.herrera88@gmail.com>
2021-09-20 15:24:01 -0600

    Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,13 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    /*
+      int num1 = 2;
+      int num2 = 4;
+      int num3 = 6;
+      int average = (num1 + num2 + num3) /3;
+      System.out.println(average);
+    */
+  }
+}
