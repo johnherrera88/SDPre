1713f2ed23ca5e56c1f597210effeb4bf3c7bf72
johnherrera88 <john.herrera88@gmail.com>
2021-09-22 15:36:29 -0600

    Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,15 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int counter = 20;
+    int sum = 0;
+    int a = 1;
+
+    while(a <= counter) {
+    sum = sum + a;
+    a++;
+}
+
+      System.out.println(sum);
+
+    }
+  }
