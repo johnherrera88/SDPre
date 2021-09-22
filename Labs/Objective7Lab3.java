58032b02e87aac040ff29e849183ea2ac2fed83a
johnherrera88 <john.herrera88@gmail.com>
2021-09-22 14:23:11 -0600

    Objective7Lab3

---
+++ Labs/Objective7Lab3.java
@@ -0,0 +1,17 @@
+public class Objective7Lab3 {
+  public static void main(String[] args) {
+    int counter = 0;
+
+    while(counter <= 19) {
+    counter = counter + 1;
+
+    if(counter % 2 == 0) {
+       System.out.println(counter + " is even ");
+     }
+
+    if(counter % 2 >= 1) {
+       System.out.println(counter + " is odd ");
+     }
+  }
+ }
+}
