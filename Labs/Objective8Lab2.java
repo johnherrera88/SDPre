74e3a3f6f79d75afb9eaf8f8753fb7e34a7057a7
johnherrera88 <john.herrera88@gmail.com>
2021-09-23 14:02:01 -0600

    Objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,12 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+  int b = 20;
+  int sum = 0;
+
+  for(int a=1; a<=b; a++){
+    sum = a + sum;
+ System.out.println(sum);
+}
+
+    }
+}
