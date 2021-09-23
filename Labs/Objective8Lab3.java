df056e9b53be556b301da12483828dd04d9a317e
johnherrera88 <john.herrera88@gmail.com>
2021-09-23 14:20:06 -0600

    Objective8Lab3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,15 @@
+public class Objective8Lab3 {
+  public static void main(String[] args) {
+
+    for (int control = 1; control <=20; control++){
+     if(control % 2 == 0)
+         System.out.println(control + " is even ");
+
+     if (control % 2 >= 1)
+         System.out.println(control + " is odd ");
+
+
+    }
+
+  }
+}
