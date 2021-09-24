18bc8cb68b08b85f21b79010cfc2d997f2cc66de
johnherrera88 <john.herrera88@gmail.com>
2021-09-24 11:34:42 -0600

    Objective7Lab5

---
+++ Labs/Objective7Lab5.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Objective7Lab5 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int selection;
+
+    while (true) {
+      System.out.println("_____Menu_____");
+      System.out.println("1: Say Hello");
+      System.out.println("2: List My favorite foods");
+      System.out.println("3: Exit");
+      System.out.println();
+
+
+
+      if (true){
+      System.out.println("1 \nHello Human");
+      System.out.println("_____Menu_____");
+      System.out.println("1: Say Hello");
+      System.out.println("2: List My favorite foods");
+      System.out.println("3: Exit");
+      System.out.println();
+
+    }
+    if (true){
+      System.out.println("2 \nApples, Bananas, Coconuts");
+      System.out.println("_____Menu_____");
+      System.out.println("1: Say Hello");
+      System.out.println("2: List My favorite foods");
+      System.out.println("3: Exit");
+      System.out.println();
+
+    }
+    if (true){
+      System.out.println("3 \nGoodbye");
+      selection = scanner.nextInt();
+    }
+  }
+}
+}
